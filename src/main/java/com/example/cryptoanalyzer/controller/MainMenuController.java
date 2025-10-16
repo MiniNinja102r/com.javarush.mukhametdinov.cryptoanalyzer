b32d3e34fa56278c26555fb511abe3d0331af38d
MiniNinja102r <vadimmuxametdinov5@gmail.com>
2025-10-17 01:36:10 +0300

    Разработка дизайна

--- src/main/java/com/example/cryptoanalyzer/controller/MainMenuController.java
+++ src/main/java/com/example/cryptoanalyzer/controller/MainMenuController.java
@@ -1,14 +1,35 @@
 package com.example.cryptoanalyzer.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public final class MainMenuController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button decryptButton;
+
+    @FXML
+    private Button encryptButton;
+
+    @FXML
+    private TextField keyField;
+
+    @FXML
+    private TextField pathField;
+
+    @FXML
+    void onDecryptClick(ActionEvent event) {
+        //
+    }
+
+    @FXML
+    void onEncryptClick(ActionEvent event) {
+        //
+    }
+
+    public void initialize() {
+        //
     }
 }
